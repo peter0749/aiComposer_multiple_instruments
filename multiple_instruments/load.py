1eaebd3800f67bf46b113a724502cedadcd439b0
jengku <jengku@gmail.com>
2017-09-16 09:21:18 +0800

    foo

--- multiple_instruments/load.py
+++ multiple_instruments/load.py
@@ -6,7 +6,7 @@ config.gpu_options.allow_growth = True
 session = tf.Session(config=config)
 from keras.models import Sequential, load_model, Model
 from keras.layers import Dense, Activation, Dropout, Input, Flatten, Conv1D
-from keras.layers import GRU, BatchNormalization, RepeatVector, TimeDistributed
+from keras.layers import LSTM, GRU, BatchNormalization, RepeatVector, TimeDistributed
 from keras.layers.merge import concatenate
 from keras.optimizers import RMSprop
 from keras.utils.io_utils import HDF5Matrix
@@ -32,7 +32,7 @@ hidden_delta=256
 hidden_note=256
 hidden_inst=256
 filter_size=128
-kernel_size=7 ## midi program changes are by groups
+kernel_size=3 ## midi program changes are by groups
 drop_rate=0.2 ## for powerful computer
 
 K.set_floatx(compute_precision);
@@ -42,49 +42,40 @@ print('Build model...')
 # network:
 with tf.device('/gpu:0'):
     noteInput  = Input(shape=(segLen, vecLen))
-    noteEncode = SoftAttentionBlock(noteInput, segLen, vecLen)
+    noteEncode = GRU(hidden_note, return_sequences=True, dropout=drop_rate)(noteInput)
     noteEncode = GRU(hidden_note, return_sequences=True, dropout=drop_rate)(noteEncode)
-    noteEncode = GRU(hidden_note, return_sequences=True, dropout=drop_rate)(noteEncode)
-    noteEncode = GRU(hidden_note, return_sequences=False, dropout=drop_rate)(noteEncode)
 
 with tf.device('/gpu:1'):
     deltaInput = Input(shape=(segLen, maxdelta))
-    deltaEncode = SoftAttentionBlock(deltaInput, segLen, maxdelta)
+    deltaEncode = GRU(hidden_delta, return_sequences=True, dropout=drop_rate)(deltaInput)
     deltaEncode = GRU(hidden_delta, return_sequences=True, dropout=drop_rate)(deltaEncode)
-    deltaEncode = GRU(hidden_delta, return_sequences=True, dropout=drop_rate)(deltaEncode)
-    deltaEncode = GRU(hidden_delta, return_sequences=False, dropout=drop_rate)(deltaEncode)
 
 with tf.device('/gpu:2'):
     instInput = Input(shape=(segLen, maxinst))
     instEncode   = Conv1D(filters=filter_size, kernel_size=kernel_size, padding='same', input_shape=(segLen, maxinst), activation = 'relu')(instInput)
-    instEncode   = SoftAttentionBlock(instEncode, segLen)
     instEncode   = GRU(hidden_inst, return_sequences=True, dropout=drop_rate)(instEncode)
     instEncode   = GRU(hidden_inst, return_sequences=True, dropout=drop_rate)(instEncode)
-    instEncode   = GRU(hidden_inst, return_sequences=False, dropout=drop_rate)(instEncode)
 
 with tf.device('/gpu:3'):
     codec = concatenate([noteEncode, deltaEncode, instEncode], axis=-1) ## return last state
+    codec = SoftAttentionBlock(codec, segLen, hidden_note+hidden_delta+hidden_inst, True)
+    codec = LSTM(hidden_note+hidden_delta+hidden_inst, return_sequences=True, dropout=drop_rate, activation='softsign')(codec)
+    codec = LSTM(hidden_note+hidden_delta+hidden_inst, return_sequences=False, dropout=drop_rate, activation='softsign')(codec)
+    codec = Dropout(drop_rate)(codec)
 
-    batchNormNote_0 = BatchNormalization()(codec)
-    fc_notes = Dense(vecLen*2, kernel_initializer='normal', activation='relu')(batchNormNote_0)
-    fc_notes = BatchNormalization()(fc_notes)
+    fc_notes = BatchNormalization()(codec)
     pred_notes = Dense(vecLen, kernel_initializer='normal', activation='softmax', name='note_output')(fc_notes) ## output PMF
 
-    batchNormDelta_0 = BatchNormalization()(codec)
-    fc_delta = Dense(maxdelta*2, kernel_initializer='normal', activation='relu')(batchNormDelta_0)
-    fc_delta = BatchNormalization()(fc_delta)
+    fc_delta = BatchNormalization()(codec)
     pred_delta = Dense(maxdelta, kernel_initializer='normal', activation='softmax', name='time_output')(fc_delta) ## output PMF
 
-    batchNormInst_0 = BatchNormalization()(codec)
-    fc_inst = Dense(maxinst*2, kernel_initializer='normal', activation='relu')(batchNormInst_0)
-    fc_inst = BatchNormalization()(fc_inst)
+    fc_inst = BatchNormalization()(codec)
     pred_inst = Dense(maxinst, kernel_initializer='normal', activation='softmax', name='inst_output')(fc_inst) ## output PMF
 
-    batchNormPower_0 = BatchNormalization()(codec)
-    fc_power = Dense(maxpower, kernel_initializer='normal', activation='relu')(batchNormPower_0)
-    fc_power = BatchNormalization()(fc_power)
+    fc_power = BatchNormalization()(codec)
     pred_power = Dense(1, kernel_initializer='normal', activation='relu', name='power_output')(fc_power) ## output regression >= 0
 aiComposer = Model([noteInput, deltaInput, instInput], [pred_notes, pred_delta, pred_inst, pred_power])
 if ( os.path.isfile('./top_weight.h5')):  ## fine-tuning
     aiComposer.load_weights('./top_weight.h5')
 aiComposer.save('./multi.h5')
+
