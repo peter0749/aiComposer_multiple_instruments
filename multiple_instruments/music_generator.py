692847690be60e859cac521995fecc599b35e6b1
jengku <jengku@gmail.com>
2017-10-01 12:16:54 +0800

    change default arg

--- multiple_instruments/music_generator.py
+++ multiple_instruments/music_generator.py
@@ -20,7 +20,7 @@ parser.add_argument('--note_temp', type=float, default=0.7, required=False,
                     help='Temperture of notes.')
 parser.add_argument('--delta_temp', type=float, default=0.7, required=False,
                     help='Temperture of time.')
-parser.add_argument('--temp_sd', type=float, default=0.05, required=False,
+parser.add_argument('--temp_sd', type=float, default=0.01, required=False,
                     help='Standard deviation of temperture.')
 parser.add_argument('--finger_number', type=int, default=5, required=False,
                     help='Maximum number of notes play at the same time.')
