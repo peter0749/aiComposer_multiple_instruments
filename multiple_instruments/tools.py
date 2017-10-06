aa48a1195299d9951e2b0bdd8d90b734d6a863b2
jengku <jengku@gmail.com>
2017-10-06 10:05:22 +0800

    test

--- multiple_instruments/tools.py
+++ multiple_instruments/tools.py
@@ -14,19 +14,6 @@ vecLen=97
 maxdelta=33 ## [0, 32]
 defaultRes=16.0
 
-# Sorted x
-def purge(x):
-    y = [x[-1]] #pick last element
-    i = len(x)-2 # iterate over all set in reversed order
-    while i>=0:
-        while i>=0 and x[i][1] == y[-1][1]: ## overlapped, find next different note
-            i -= 1
-        if i>=0: ## founded
-            y.append(x[i])
-        i -= 1
-    return list(reversed(y))
-# merge some overlaped intervals into single interval
-
 def Tempo2BPM(x):
     ret = x.data[2] | x.data[1]<<8 | x.data[0]<<16
     ret = float(60000000)/float(ret)
@@ -36,9 +23,9 @@ def Tempo2BPM(x):
 def pattern2map(pattern, maxtick):
     if pattern.format!=1 or len(pattern)==0: raise Exception('Wrong file format!')
     ResScale = float(pattern.resolution) / float(defaultRes)
-    data=[(0.0, 0)]#tick , key (main+accompany)
+    data=[(0, 0)]#tick , key (main+accompany)
     for track in pattern: ## main melody if instrument==0 else accompany
-        temp=[(0.0, 0)] #tick, note, instrument
+        temp=[(0, 0)] #tick, note, instrument
         speedRatio = 1.0
         Normal = 120.0
         accumTick = 0.
@@ -55,20 +42,19 @@ def pattern2map(pattern, maxtick):
                 speedRatio = float(changeBPM)/float(Normal)
             elif isinstance(v, midi.ProgramChangeEvent):
                 if v.channel==9 or v.data[0] >= 24:  ## is drum or is not keyboard instrument
-                    temp=[(0.0, 0)]
+                    temp=[(0, 0)]
                     break
             elif isinstance(v, midi.NoteOnEvent) and v.data[1]>0:
                 note = v.data[0]
-                temp.append((accumTick, note))
+                tick = int(round(accumTick/ResScale))
+                temp.append((tick, note))
         temp = temp[1:-1]
         data.extend(temp)
     data = list(set(data)) ## remove duplicate data
     if len(data)<2: raise Exception('No events!')
     data.sort()
-    #data = purge(data)
     for i in range(0, len(data)-1):
         tick = data[i+1][0] - data[i][0]
-        tick = int(round(tick/ResScale)) ## adjust resolution, downsampling
         tick = maxtick if tick>maxtick else tick ## set a threshold
         note = (0 if i==0 else data[i+1][1] - data[i][1]) + 48
         while note<0: note+=12
